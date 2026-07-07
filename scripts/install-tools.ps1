e563898947d4e08c82b70c0660cf29fc12cbbc07
Rafael Kaher <83352373+Rafakka@users.noreply.github.com>
2026-07-07 12:22:38 -0300

    Update install-tools.ps1

--- scripts/install-tools.ps1
+++ scripts/install-tools.ps1
@@ -1,1 +1,24 @@
 
+if (-not (Test-Command "rg")) {
+
+    winget install BurntSushi.ripgrep
+
+}
+
+if (-not (Test-Command "fd")) {
+
+    winget install sharkdp.fd
+
+}
+
+if (-not (Test-Command "tree-sitter")) {
+
+    npm install -g tree-sitter-cli
+
+}
+
+if (-not (Test-Command "nvim")) {
+
+    winget install Neovim.Neovim
+
+}
