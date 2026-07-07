0853e7b8a507882e111e953d9e6645a8c3f0f99e
Rafael Kaher <83352373+Rafakka@users.noreply.github.com>
2026-07-07 12:02:16 -0300

    Refactor LSP configuration to use require

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -2,9 +2,7 @@ return
 {
     "neovim/nvim-lspconfig",
     
-    config = function()
-        vim.lsp.enable("pyright")
-        vim.lsp.enable("vtsls")
-    end,
-
+   config = function()
+        require("lsp")
+end
     }
