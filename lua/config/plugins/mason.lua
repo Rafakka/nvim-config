03f51e519ba100d414d29085d7e7514b67297376
Rafael Kaher <83352373+Rafakka@users.noreply.github.com>
2026-07-07 11:39:02 -0300

    Update mason.lua

--- lua/config/plugins/mason.lua
+++ lua/config/plugins/mason.lua
@@ -1,6 +1,29 @@
-return
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-        dependencies = {
+return {
+    {
         "williamboman/mason.nvim",
-        }
+
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    "pyright",
+                    "vtsls",
+                },
+
+                run_on_start = true,
+                auto_update = false,
+            })
+        end,
+    },
+}
