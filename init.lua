175b08ea5b83c95a5c37b787768b538a7c19d847
Rafakka <rafaelkaher@gmail.com>
2026-07-16 10:41:57 -0300

    melhorado ordem de load

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-require("config.keymaps")
-require("config.lazy")
 require("config.options")
+require("config.keymaps")
 require("config.autocmds")
+require("config.lazy")
